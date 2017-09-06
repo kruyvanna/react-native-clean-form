433af4d321c08643c108610562a67f195375ed69
Kruy Vanna <kruyvanna.io@gmail.com>
2017-09-06 14:00:07 +0700

    fix onBlur for input causing asyncvalidation to have undefined value

--- src/redux-form/createInputs.js
+++ src/redux-form/createInputs.js
@@ -12,8 +12,8 @@ import {
 } from '../../index'
 
 const createInputs = inputCreator => {
-  const renderInput = ({ input: { onChange, ...restInput }, placeholder}) => (
-    <InputRenderer onChangeText={onChange} placeholder={placeholder} {...restInput} />
+  const renderInput = ({ input: { onChange, onBlur, value, ...restInput }, placeholder}) => (
+    <InputRenderer onChangeText={onChange} onBlur={()=>onBlur(value)} placeholder={placeholder} {...restInput} />
   )
   const Input = inputCreator('Input', renderInput, InputRenderer.PropTypes, InputRenderer.defaultProps)
 
